9cb8062edf6d2b551d3eeba4636d2c416dbc5321
Leonardo <146593453+LeonS08@users.noreply.github.com>
2024-04-30 18:23:01 -0300

    trabajo practico terminado
    ejercicio completado

--- practicos/42 - 55883 - Pedraza, Leonardo Miguel/TP1/index.jsx
+++ practicos/42 - 55883 - Pedraza, Leonardo Miguel/TP1/index.jsx
@@ -1,7 +1,21 @@
+let contactos = [
+    { id: 1, nombre: 'Elena Rodríguez', telefono: '5551234567' },
+    { id: 2, nombre: 'Tomás Herrera', telefono: '3129876543' },
+    { id: 3, nombre: 'Sofia Gutiérrez', telefono: '4085617890' },
+    { id: 4, nombre: 'Leonardo Vázquez', telefono: '2124659821'}
+];
+
 const App = () =>  (
     <div>
-        <h1>¡Hola, mundo!</h1>
-        <p>¡Bienvenidos a React!</p>
+        <h1>Contactos</h1>
+        <ul>
+        {contactos.map(contacto => (
+          <li class="contactos" key={contacto.id}>
+            <div className="nombre del contacto">Nombre: {contacto.nombre}</div>
+            <div className="numero de contacto">Telefono: {contacto.telefono}</div>
+          </li>
+        ))}
+      </ul>
     </div>
 )
 
